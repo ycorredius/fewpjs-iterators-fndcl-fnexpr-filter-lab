fac246044f2235e00b309120d0af10be454c2f74
shaun <ycorredius@gmail.com>
2020-03-06 12:36:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(someArray,someString){
+    return someArray.filter(n =>{
+        return n.toLowerCase() == someString.toLowerCase()
+    })
+}
+
+function fuzzyMatch(someArray,someString){
+    return someArray.filter(n => 
+        n.toLowerCase().indexOf(someString.toLowerCase()) === 0
+    )
+}
+
+function matchName(someArray,someString){
+    return someArray.filter(n => 
+        n.name === someString
+    )
+}
